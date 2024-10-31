4f3b2e62b901b68093e50530858045c3391ce152
Mirali-Shadow <amiralimhmdi1386@gmail.com>
2024-10-31 17:56:08 +0000

    1

--- bot.py
+++ bot.py
@@ -1,25 +1,18 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
-# مسیر فایل مورد نظر (مطمئن شوید که فایل در همان دایرکتوری وجود دارد یا مسیر کامل آن را بنویسید)
-FILE_PATH = "https://github.com/MIRALILOR/MyFile/blob/d19550a1a44891fc9dff7f485d80df17e60f0412/MyMusic/Niloofar%20Abi.mp3"
-TOKEN = '6414679474:AAHBrTFt5sCbbudkXHu3JvPrR_Pj50T30qs'
+# توکن بات شما
+TOKEN = "6414679474:AAHBrTFt5sCbbudkXHu3JvPrR_Pj50T30qs"
 
-# تابع استارت که فایل را ارسال می‌کند
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # ارسال فایل به کاربر
-    await update.message.reply_document(document=open(FILE_PATH, 'rb'))
+# تابع برای ارسال فایل به کاربر
+async def start(update: Update, context: CallbackContext) -> None:
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=open("https://github.com/Mirali-Shadow/MyFile/blob/1da71e7d2321d5224725caefceb6571ccea67962/Pishro%20-%20Tamum%20Shode%20(featuring%20Kamyar).mp3", 'rb'))
 
-async def main() -> None:
-    # ساخت اپلیکیشن ربات با توکن
+# راه‌اندازی و افزودن هندلرها
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
-
-    # افزودن هندلر برای دستور استارت
     app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-    # شروع به دریافت به‌روزرسانی‌ها
-    await app.run_polling()
-
-if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+if __name__ == "__main__":
+    main()
