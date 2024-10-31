4546ced4bf901fec87cbf78be55f5cb192b5e3fd
Mirali-Shadow <amiralimhmdi1386@gmail.com>
2024-10-31 18:31:22 +0000

    66

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
-from telegram import Update, ParseMode
+from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
 # توکن بات شما
