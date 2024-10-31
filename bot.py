449e5d07f894d931aa873978e06f1e15f8b6b998
Mirali-Shadow <amiralimhmdi1386@gmail.com>
2024-10-31 16:28:20 +0000

    2

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 # توکن ربات خود را در اینجا قرار دهید
 TOKEN = '6414679474:AAHBrTFt5sCbbudkXHu3JvPrR_Pj50T30qs'  # توکن ربات شما
@@ -8,30 +8,29 @@ TOKEN = '6414679474:AAHBrTFt5sCbbudkXHu3JvPrR_Pj50T30qs'  # توکن ربات ش
 file_url = 'https://raw.githubusercontent.com/Mirali-Shadow/MyFile/2fa16293c554cc38e87fa56831e94d52f0ef3a5d/Seft%20(Djsajjad1%20%26%20BLH%20Remix).mp3'
 
 # تابعی برای ارسال سلام هنگام استارت ربات
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('سلام! خوش آمدید به ربات ما!')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('سلام! خوش آمدید به ربات ما!')
 
 # تابعی برای مدیریت پیام‌ها
-def handle_message(update: Update, context: CallbackContext):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat.id
 
     # چک کردن آیا پیام شامل لینک اختصاصی است
     if update.message.text == 'https://t.me/shadow_byte_bot?start=getfile':
         # ارسال فایل به کاربر
-        context.bot.send_document(chat_id, file_url)
+        await context.bot.send_document(chat_id, file_url)
 
 # تابع اصلی برای اجرای ربات
 def main():
-    # ایجاد آپدیت و دریافت بدون استفاده از use_context
-    updater = Updater(TOKEN)
+    # ایجاد شیء Application
+    application = ApplicationBuilder().token(TOKEN).build()
 
     # ثبت هندلرها
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # شروع ربات
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
