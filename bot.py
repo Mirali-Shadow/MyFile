dac03a480da76f50bff80a0c34e57e8d8cdde673
Mirali-Shadow <amiralimhmdi1386@gmail.com>
2024-10-31 19:53:27 +0000

    2

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext, Me
 
 # توکن بات شما
 TOKEN = "6414679474:AAHBrTFt5sCbbudkXHu3JvPrR_Pj50T30qs"
-CHANNEL_USERNAME = "Mirali_Vibe"  # نام کاربری کانال شما را در اینجا قرار دهید
+CHANNEL_USERNAME = "@mirali_vibe"  # نام کاربری کانال شما را در اینجا قرار دهید
 
 # تابع برای ارسال پیام خوش آمدگویی
 async def start(update: Update, context: CallbackContext) -> None:
@@ -27,13 +27,16 @@ async def check_membership(update: Update, context: CallbackContext) -> None:
     await query.answer()  # پاسخ به callback query
 
     user_id = query.from_user.id  # شناسه کاربر
-    chat_member = await context.bot.get_chat_member(CHANNEL_USERNAME, user_id)
-
-    if chat_member.status in ['member', 'administrator', 'creator']:
-        await query.message.reply_text("شما در کانال عضو هستید. فایل ارسال خواهد شد.")
-        await send_file(user_id, context)  # ارسال فایل
-    else:
-        await query.message.reply_text("شما در کانال عضو نیستید. لطفاً ابتدا عضو شوید.")
+    try:
+        chat_member = await context.bot.get_chat_member(CHANNEL_USERNAME, user_id)
+        
+        if chat_member.status in ['member', 'administrator', 'creator']:
+            await query.message.reply_text("شما در کانال عضو هستید. فایل ارسال خواهد شد.")
+            await send_file(user_id, context)  # ارسال فایل
+        else:
+            await query.message.reply_text("شما در کانال عضو نیستید. لطفاً ابتدا عضو شوید.")
+    except Exception as e:
+        await query.message.reply_text(f"خطا در بررسی عضویت: {str(e)}")  # پیام خطا در صورت بروز مشکل
 
 # تابع برای ارسال فایل
 async def send_file(user_id: int, context: CallbackContext):
