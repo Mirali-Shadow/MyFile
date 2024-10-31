3ea8b1d56b09061158d75d94083b48f8dd48ca63
Mirali-Shadow <amiralimhmdi1386@gmail.com>
2024-10-31 15:10:57 +0000

    1

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // توکن ربات شما
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '6414679474:AAHBrTFt5sCbbudkXHu3JvPrR_Pj50T30qs';
 const bot = new TelegramBot(token, { polling: true });
 
 // پاسخ به پیام "/start"
